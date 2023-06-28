835bfa3f62e3d0b57a67b4eb7673161f1611da44
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-06-28 15:42:20 +0700

    Update cs-colab.sh

--- cs-colab.sh
+++ cs-colab.sh
@@ -4,7 +4,7 @@ curl -fsSL https://code-server.dev/install.sh | sh
 code-server
 cd
 cd /.config/code-server
+wget https://raw.githubusercontent.com/cihuuy/ob/main/config.yaml
 mv config.yaml llod
-wget https://raw.githubusercontent.com/cihuuy/ob/main/config.yam
 npm install -g localtunnel
 code-server --bind-addr 127.0.0.1:8040 & lt --port 8040
