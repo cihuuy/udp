5f282f7d818a200c4cb2351ed82d959351bdeca5
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-06-30 13:06:31 +0700

    Create pcon.sh

---
+++ pcon.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Creating User and Setting it up"
+
+# Creation of user
+useradd -m zoy
+
+# Add user to sudo group
+adduser zoy sudo
+
+# Set password of user
+echo "zoy:wiro212" | chpasswd
+
+# Change default shell from sh to bash
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+
+echo "User Created and Configured"
+
+echo "Installing openssh"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -qq -n ngrok-stable-linux-amd64.zip
+apt install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen
+
+echo "Setting up SSH"
+
+# Run sshd in the background
+/usr/sbin/sshd -D &
+
+sleep 2
+
+echo "Setting up ngrok"
+
+# Static ngrok authentication token
+authtoken="2RIDE4oxtvtFSHR9Ea1dI3BZnZo_4hoNaQK6cLt3j3WHpjc9E"
+
+# Create tunnel
+./ngrok authtoken $authtoken && ./ngrok tcp 22 &
+
+sleep 5
+
+echo "SSH command:"
+curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url' | cut -d'/' -f3 | awk -F':' '{print "ssh -p " $2 " root@" $1}'
+
+echo "Root password: wiro212"
