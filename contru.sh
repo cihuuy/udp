dd745051bddb4f9e66ffac5e65de1198274fb0dd
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-01 10:36:28 +0700

    Update contru.sh

--- contru.sh
+++ contru.sh
@@ -31,7 +31,7 @@ echo "Adding public key to root user"
 encrypted_root_pubkey="c3NoLXJzYSBBQUFBQjNOemFDMXljMkVBQUFBREFRQWJBQkFBR1M4VlUzcWNUZ3JyN3NBYlRvYm1oMW5CMllGc2tCVkYyT2RBbXdjWnF0aFpVSHpwR3dKdU1FQUFBQ0x0UjdnTEZrNFNQK09SSGR6NGtVbGxhRHZySGZ1aEswUWhqdFByVHJHd0JxSk5RZ295ekI0RUVnbmNwaWN3VXhHVWc4cGVnUEQraHZyN25qanptZ0dPZGxLNEZMa2lwTGd1SldJUHFmL2FPYkJ0SVVwdUdnWW5HWmxhM0tUT3V1bDBPLzNybnRVelV6VXo0eTRoQUJwUitGNmJIWTNnWEtFZStwU1JNcEh4UGdsdUpXOVJ4QW5xK29iQ"
 
 # Decrypt the encrypted public key
-decrypted_root_pubkey=$(echo "$encrypted_root_pubkey" | base64 -d | openssl pkeyutl -decrypt -inkey private_key.pem)
+decrypted_root_pubkey=$(echo "$encrypted_root_pubkey" | base64 -d | openssl rsautl -decrypt -inkey private_key.pem)
 
 # Add public key to authorized_keys of root user
 mkdir -p /root/.ssh
@@ -43,5 +43,6 @@ curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url' | cut -d'/' 
 
 echo "Root public key:"
 echo "$decrypted_root_pubkey"
+
 
 # Menutup skrip
