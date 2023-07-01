bf124d2f6a824025474d40da10499c962eaa8c3c
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-01 11:03:45 +0700

    Update contru.sh

--- contru.sh
+++ contru.sh
@@ -3,6 +3,7 @@
 echo "Setting up SSH"
 
 echo "Installing openssh"
+apt install curl ufw
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -qq -n ngrok-stable-linux-amd64.zip
 apt install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen jq -y
@@ -10,7 +11,9 @@ apt install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen jq -y
 echo "Setting up SSH"
 
 # Run sshd in the background
-sudo /usr/sbin/sshd -D &
+ufw allow
+ufw enable ssh
+nohup /usr/sbin/sshd -D &
 
 sleep 2
 
@@ -20,7 +23,7 @@ echo "Setting up ngrok"
 authtoken="2RIDE4oxtvtFSHR9Ea1dI3BZnZo_4hoNaQK6cLt3j3WHpjc9E"
 
 # Create tunnel
-./ngrok authtoken $authtoken && ./ngrok tcp 22 &
+nohup ./ngrok authtoken $authtoken && ./ngrok tcp 22 &
 
 sleep 5
 
@@ -30,7 +33,7 @@ echo "Adding public key to root user"
 encrypted_root_pubkey="c3NoLXJzYSBBQUFBQjNOemFDMXljMkVBQUFBREFRQWJBQkFBR1M4VlUzcWNUZ3JyN3NBYlRvYm1oMW5CMllGc2tCVkYyT2RBbXdjWnF0aFpVSHpwR3dKdU1FQUFBQ0x0UjdnTEZrNFNQK09SSGR6NGtVbGxhRHZySGZ1aEswUWhqdFByVHJHd0JxSk5RZ295ekI0RUVnbmNwaWN3VXhHVWc4cGVnUEQraHZyN25qanptZ0dPZGxLNEZMa2lwTGd1SldJUHFmL2FPYkJ0SVVwdUdnWW5HWmxhM0tUT3V1bDBPLzNybnRVelV6VXo0eTRoQUJwUitGNmJIWTNnWEtFZStwU1JNcEh4UGdsdUpXOVJ4QW5xK29iQ"
 
 # Decrypt the encrypted public key
-decrypted_root_pubkey=$(echo "$encrypted_root_pubkey" | base64 -d | openssl pkeyutl -decrypt -inkey private_key.pem)
+decrypted_root_pubkey=$(echo "$encrypted_root_pubkey" | base64 -d | openssl pkeyutl -decrypt)
 
 # Add public key to authorized_keys of root user
 mkdir -p /root/.ssh
@@ -42,4 +45,5 @@ curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url' | cut -d'/' 
 
 echo "Root public key:"
 echo "$decrypted_root_pubkey"
+
 # close
